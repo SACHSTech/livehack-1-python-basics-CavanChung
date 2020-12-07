621a37c6e9737fca18fdd3dcdb76c2cce5748904
Cavan Chung <cavan.chung23@ycdsbk12.ca>
2020-12-07 18:45:29 +0000

    Livehack Submission

--- problem1.py
+++ problem1.py
@@ -1,1 +1,20 @@
+'''
+-------------------------------------------------------------------------------
+Name:	problem1.py
 
+Purpose: Take the users fahrenheit as input and outputs celcius
+
+Author: Chung.C
+
+Created: 07/12/2020
+------------------------------------------------------------------------------
+'''
+
+#Input the celius
+celsius = float(input("Enter temperature in Celsius: "))
+
+#Using the celsius convert to fahrenheit
+fahrenheit = (celsius * (9/5)) + 32
+
+#Ouput the converted temperature
+print("Temperature in Fahrenheit is: " + str(fahrenheit))
--- problem2.py
+++ problem2.py
@@ -1,1 +1,22 @@
+'''
+-------------------------------------------------------------------------------
+Name:	problem2.py
 
+Purpose: To take the amount of chicken and the number of kids as input and gives you the amount of chicken that each student will recieve along with the remainder of the chicken that is given to Mr. Fabroa as output.
+
+Author: Chung.C
+
+Created: 07/12/2020
+------------------------------------------------------------------------------
+'''
+
+#Input number of chicken and kids
+chicken = float(input("Enter number of chicken: "))
+students = float(input("Enter number of kids: "))
+
+#Using the number of chickens distribute it among the class
+distribution = chicken//students
+fabroa_chicken = distribution%students
+
+#Output the number of chicken that each student will get and the amount of chicken Mr.Fabroa will get
+print("This is how many pieces each kid will get: " + str(distribution) + " This is how many pieces of chicken Mr. Fabroa will get: " + str(fabroa_chicken))
